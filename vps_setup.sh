012e331ff0af744059d07df9f79151549072f39a
Rami Al-Ghanmi <rami@ramialghanmi.com>
2013-05-25 15:04:09 -0700

    Update script to work with Debian 7.0 Wheezy:
      + Replaced lighttpd with Nginx
      + Changed Repositoried to work with Wheezy
      + Update backports with new format
      + Added Nginx repo
      + Switched from using apt-get to aptitude
      + Network interfaces now use static interface instead of DHCP
      + Added support for IPv6. This is signified by providing an IPv6 IP. The following is updated with IPv6 info:
        * Firewall (ip6tables)
        * Network Interfaces
        * DNS Entries
        * Hostnames

--- vps_setup.sh
+++ vps_setup.sh
@@ -80,20 +80,28 @@ print_prompt
 print_log "Updating Repositories"
 print_prompt
 cp /etc/apt/sources.list /etc/apt/sources.list.default
-echo "deb http://ftp.us.debian.org/debian squeeze main contrib non-free" | tee /etc/apt/sources.list
-echo "deb http://security.debian.org/ squeeze/updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb http://ftp.us.debian.org/debian/ wheezy main contrib non-free" | tee /etc/apt/sources.list
+echo "deb http://ftp.us.debian.org/debian/ wheezy-updates main contrib non-free" | tee -a /etc/apt/sources.list
+echo "deb http://security.debian.org/ wheezy/updates main contrib non-free" | tee -a /etc/apt/sources.list
+
+## Debian Packports
 echo "# Debian Backports" | tee /etc/apt/sources.list.d/debian-backports.list
-echo "deb http://backports.debian.org/debian-backports squeeze-backports main" | tee -a /etc/apt/sources.list.d/debian-backports.list
+echo "deb http://ftp.debian.org/debian/ wheezy-backports main" | tee -a /etc/apt/sources.list.d/debian-backports.list
+
+## Nginx Official Package
+echo "# Nginx" | tee /etc/apt/sources.list.d/nginx.list
+echo "deb http://nginx.org/packages/debian/ wheezy nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+apt-key adv --keyserver pgp.mit.edu --recv-keys ABF5BD827BD9BF62
 
 ## Update system
 print_log "Package update"
-apt-get update
-apt-get upgrade
-apt-get dist-upgrade
+aptitude update
+aptitude upgrade
+aptitude dist-upgrade
 
 ## Install new packages
 print_log "Installing new packages"
-echo -n "apt-get install " > $PACKAGES_SCRIPT
+echo -n "aptitude install " > $PACKAGES_SCRIPT
 sed '/^\#/d;/^$/d' $PACKAGES_FILE | tr '\n' ' ' >> $PACKAGES_SCRIPT
 chmod 755 $PACKAGES_SCRIPT
 sh $PACKAGES_SCRIPT
@@ -110,8 +118,9 @@ adduser $SUPER_USER
 usermod -a -G sudo $SUPER_USER
 usermod -a -G adm $SUPER_USER
 usermod -a -G www-data $SUPER_USER
+addgroup developers
 # Create Git User
-adduser git
+adduser --disabled-password git
 usermod -a -G www-data git
 usermod -a -G developers git
 
@@ -143,18 +152,43 @@ print_log "Updating alernatives"
 update-alternatives --config editor
 #update-alternatives --config java
 
+## Network Interfaces
+print_log "Static IP Address Configuration"
+cp /etc/network/interfaces /etc/network/interfaces.default
+sed -i "s/iface eth0 inet dhcp/iface eth0 inet static\n\taddress $SERVER_IP\n\tnetmask $SERVER_NETMASK\n\tgateway $SERVER_GATEWAY\n/" /etc/network/interfaces
+if [ -n "$SERVER_IPv6" ]; then
+	sed -i "s/iface eth0 inet6 dhcp/iface eth0 inet6 static\n\taddress $SERVER_IPv6\n\tnetmask $SERVER_NETMASKv6\n\tgateway $SERVER_GATEWAYv6/" /etc/network/interfaces
+fi
+
+
 ## Hostname
 print_log "HostName configuration"
 echo "$SERVER_NAME.$SERVER_DOMAIN" | tee /etc/hostname
 hostname -F /etc/hostname
+
+## IPv4
 echo -e "127.0.0.1\tlocalhost.localdomain localhost" | tee /etc/hosts
 echo -e "$SERVER_IP\t$SERVER_NAME.$SERVER_DOMAIN $SERVER_NAME $SERVER_OTHER_NAMES" | tee -a /etc/hosts
 
+##IPv6 if defined
+if [ -n "$SERVER_IPv6" ]; then
+	echo -e "\n\n#IPv6 Configuration" | tee -a /etc/hosts
+	echo -e "::1\tlocalhost.localdomain localhost ip6-localhost ip6-loopback" | tee -a /etc/hosts
+	echo -e "$SERVER_IPv6\t$SERVER_NAME.$SERVER_DOMAIN $SERVER_NAME $SERVER_OTHER_NAMES $SERVER_OTHER_NAMES_IPv6" | tee -a /etc/hosts
+	echo -e "ff02::1\tip6-allnodes" | tee -a /etc/hosts
+	echo -e "ff02::2\tip6-allrouters" | tee -a /etc/hosts
+fi
+
 ## DNS Name Servers
 print_log "DNS Configuration"
+cp /etc/resolv.conf /etc/resolv.conf.default
 sed -i '1i\
 nameserver 8.8.8.8\
 nameserver 8.8.4.4' /etc/resolv.conf
+if [ -n "$SERVER_IPv6" ]; then
+	echo "nameserver 2001:4860:4860::8888" | tee -a /etc/resolv.conf
+	echo "nameserver 2001:4860:4860::8844" | tee -a /etc/resolv.conf
+fi
 service networking restart
 
 ## SSH Configuration
@@ -164,6 +198,9 @@ sed -i "s/Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
 sed -i -e 's/^PermitRootLogin yes/PermitRootLogin no/' -e 's/^PermitEmptyPasswords yes/PermitEmptyPasswords no/' /etc/ssh/sshd_config
 sed -i 's/^X11Forwarding yes/X11Forwarding no/' /etc/ssh/sshd_config
 sed -i "s/#ListenAddress 0.0.0.0/ListenAddress $SERVER_IP/" /etc/ssh/sshd_config
+if [ -n "$SERVER_IPv6" ]; then
+	sed -i "s/#ListenAddress ::/ListenAddress $SERVER_IPv6/" /etc/ssh/sshd_config
+fi
 sed -i 's/^UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
 sed -i 's/^UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 echo "" | tee -a /etc/ssh/sshd_config
@@ -191,6 +228,11 @@ echo "Hello World! From $USER on $(hostname) sent to $SUPER_USER" | mail -s "Hel
 curl https://raw.github.com/alghanmi/vps_setup/master/scripts/iptables-setup.sh | sed -e s/^SERVER_IP=.*/SERVER_IP=\"$SERVER_IP\"/ -e s/^SSH_PORT=.*/SSH_PORT=\"$SSH_PORT\"/ - > /home/$SUPER_USER/bin/iptables-setup.sh
 chmod 755 /home/$SUPER_USER/bin/iptables-setup.sh
 sh /home/$SUPER_USER/bin/iptables-setup.sh
+if [ -n "$SERVER_IPv6" ]; then
+	curl https://raw.github.com/alghanmi/vps_setup/master/scripts/ip6tables-setup.sh | sed -e s/^SERVER_IP=.*/SERVER_IP=\"$SERVER_IPv6\"/ -e s/^SSH_PORT=.*/SSH_PORT=\"$SSH_PORT\"/ - > /home/$SUPER_USER/bin/ip6tables-setup.sh
+	chmod 755 /home/$SUPER_USER/bin/ip6tables-setup.sh
+	sh /home/$SUPER_USER/bin/ip6tables-setup.sh
+fi
 
 ## Automatic package upgrades
 echo "APT::Periodic::Enable \"1\";" | tee /etc/apt/apt.conf.d/30auto-upgrades
@@ -198,4 +240,5 @@ echo "APT::Periodic::Update-Package-Lists \"1\";" | tee -a /etc/apt/apt.conf.d/3
 echo "APT::Periodic::AutocleanInterval \"7\";" | tee -a /etc/apt/apt.conf.d/30auto-upgrades
 echo "APT::Periodic::Unattended-Upgrade \"1\";" | tee -a /etc/apt/apt.conf.d/30auto-upgrades
 echo "Unattended-Upgrade::Mail \"$SUPPORT_EMAIL\";" | tee -a /etc/apt/apt.conf.d/30auto-upgrades
+cp /etc/apticron/apticron.conf /etc/apticron/apticron.conf.default
 sed -i "s/^EMAIL=.*/EMAIL=\"$SUPPORT_EMAIL\"/" /etc/apticron/apticron.conf
